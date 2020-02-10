d3c1ac6cd946a6d59e261f4c066db46caef8634a
ChronoSky <zik2004@mail.ru>
2020-02-10 13:14:58 +0300

    Task_1 Task_2

---
+++ src/Task_1.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>){
+
+    var any : Any? = null
+    any = 1
+    val p = any/2.5
+}
---
+++ src/Task_2.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>){
+
+    val h : String = "hello"
+    val a : String = "aplana"
+    print("$h $a")
+}
