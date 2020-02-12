50a2a33694a58e53dccef0d409edad75312741cb
ChronoSky <zik2004@mail.ru>
2020-02-12 20:42:34 +0300

    Task_4 Task_5

---
+++ src/Task_4.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+
+    println("Введите имя пользователя и нажмите Enter:")
+
+    var name : String? = readLine()
+
+    name =  name.toString()
+
+    println("Hello, $name")
+}
---
+++ src/Task_5.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+
+    val range = 'z' downTo 'a'
+    for(char in range) print(char)
+    
+}
