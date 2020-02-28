4a0b1c17cc5ca837498f68f5768898c9985f44b4
ChronoSky <zik2004@mail.ru>
2020-02-28 16:27:33 +0300

    Task_13

---
+++ src/Task_13.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var array: Array<String> = arrayOf("Иван", "Николай" , "Иван", "Петр" , "Максим")
+    count(*array)
+
+}
+fun count(vararg users: String)  {
+    val mapUserNames : MutableMap<String, Int> = mutableMapOf()
+
+    for (user in users){
+        if (mapUserNames.containsKey(user)){
+            var curCount = mapUserNames.getValue(user)
+            mapUserNames.put(user, curCount+1)
+        }else {
+            mapUserNames.put(user, 1)
+        }
+    }
+
+    for (user in mapUserNames){
+        println(user.key +" - " + user.value)
+    }
+}
